7b32c1994432c217d1487f3645dcd4694622b5be
Erutaner <renaissancezj@gmail.com>
2022-12-30 23:12:30 +0800

    实现注册功能，可以选择登录或注册，账号将被写入数据库

--- client_test.py
+++ client_test.py
@@ -5,34 +5,63 @@ Date: 2022.12.30
 import socket
 from my_arg import MY_SERVER_PORT, MY_SERVER_IP
 
+def loop_str_sending(client):
+    while True:
+        inp = input('>>>：').strip()
+        # 向服务端发送数据，需要转换成Bytes类型发送
+        client.send(inp.encode('utf-8'))
+        if inp == 'bye':
+            return  # 双层循环唯一出口
+        # 接收服务端回应给客户端的数据，不能超过1024字节
+        res = client.recv(1024)
+        print(res.decode('utf-8'))
+
+
+
 # 创建套接字对象，AF_INET基于IPV4通信，SOCK_STREAM以数据流的形式传输数据，这里就可以确定是TCP了
 client = socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
 
 # 连接服务端
 client.connect((MY_SERVER_IP,MY_SERVER_PORT))
 while True:  # 最外层循环用于登录
-    user_name = input('Please input your user name:').strip()
-    passwd = input("Please input your password:").strip()
-    client.send(user_name.encode('utf-8'))  # 将输入的用户名编码后进行传输
-    client.send(passwd.encode('utf-8'))  # 将输入的密码编码后传输
-    res = client.recv(1020)   # 接收服务器对账号密码判定的结果
-    print(res.decode('utf-8'))   # 解码后对本次登录结果输出
-    if res.decode('utf-8') == "Log in successfully":  # 若登录有效，则进入
+    user_name = input('Please input your user name, if you don\'t have one, input sign up to sign up:').strip()
+    client.send(user_name.encode('utf-8'))
+
+    # 注册模块
+    if user_name == "sign up":
         while True:
-            inp = input('>>>：').strip()
-            # 向服务端发送数据，需要转换成Bytes类型发送
-            client.send(inp.encode('utf-8'))
-            if inp == 'bye':
-                break  # 双层循环唯一出口
-            # 接收服务端回应给客户端的数据，不能超过1024字节
-            res = client.recv(1024)
+            user_name = input("Please set your user name:").strip()
+            passwd = input("Please set your password, after then you can sign in:").strip()
+            client.send(user_name.encode('utf-8'))  # 将输入的用户名编码后进行传输
+            client.send(passwd.encode('utf-8'))  # 将输入的密码编码后传输
+            sign_up_ret = client.recv(1020)
+            print(sign_up_ret.decode('utf-8'))
+            if sign_up_ret.decode('utf-8') == "Account has already existed.":
+                continue
+            else:
+                res = client.recv(1020)  # 接收服务器对账号密码判定的结果
+                print(res.decode('utf-8'))  # 解码后对本次登录结果输出
+                if res.decode('utf-8') == "Log in successfully":  # 若登录有效，则进入, 这里一定有效，其实没必要了
+                    loop_str_sending(client)
+                else:
+                    continue
+                print("Disconnected.")
+                break
 
-            print(res.decode('utf-8'))
+
+    # 登录模块
     else:
-        continue
-    print("Disconnected.")
+        passwd = input("Please input your password:").strip()
+        client.send(user_name.encode('utf-8'))  # 将输入的用户名编码后进行传输
+        client.send(passwd.encode('utf-8'))  # 将输入的密码编码后传输
+        res = client.recv(1020)   # 接收服务器对账号密码判定的结果
+        print(res.decode('utf-8'))   # 解码后对本次登录结果输出
+        if res.decode('utf-8') == "Log in successfully":  # 若登录有效，则进入
+            loop_str_sending(client)
+        else:
+            continue
+        print("Disconnected.")
+        break
     break
-
-
 # 套接字关闭
 client.close()
--- server_test.py
+++ server_test.py
@@ -6,6 +6,16 @@ import pymysql
 import socket
 from my_arg import MY_SERVER_IP, MY_SERVER_PORT
 
+def echo_str_back(conn):
+    while True:  # 以下是信息传输过程，与前一个版本一致
+        # 接收客户端传递的数据，只接收1024个字节数据
+        res = conn.recv(1024)
+        if res.decode("utf-8") == 'bye':
+            return
+        # 将客户端的数据接收到以后，转换成大写再编码后发送给客户端
+        conn.send(res.decode('utf-8').upper().encode('utf-8'))
+
+
 def log_in_test(user_name,passwd):
     # 创建连接对象
     sql_conn = pymysql.connect(host='localhost',port=3306,user='root',password='zj20030811'\
@@ -28,6 +38,32 @@ def log_in_test(user_name,passwd):
     sql_conn.close()
     return ret
 
+def newly_sign_up(user_name,passwd):
+    # 创建连接对象
+    sql_conn = pymysql.connect(host='localhost', port=3306, user='root', password='zj20030811' \
+                               , database='client_info', charset='utf8')
+    # 获取游标对象
+    cursor = sql_conn.cursor()
+    # 我再写个语句
+    user_sql = f"insert into info values('{user_name}','{passwd}');"
+    # 执行以下这个语句，注意处理错误
+    try:
+        cursor.execute(user_sql)
+        # 提交数据到数据库
+        sql_conn.commit()
+        ret = "Account created."
+    except Exception as e:
+        sql_conn.rollback()
+        ret = "Account has already existed."
+    finally:
+        # 关闭游标
+        cursor.close()
+        # 关闭连接
+        sql_conn.close()
+        return ret
+
+
+
 # 创建套接字对象，AF_INET基于IPV4通信，SOCK_STREAM以数据流的形式传输数据，这里就可以确定是TCP了
 server = socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
 
@@ -45,25 +81,41 @@ conn,ip_addr = server.accept()
 
 while True:  # 最外层循环用于登录
     # 接收客户端传递的数据，只接收1024个字节数据
-    user_name = conn.recv(1024)
-    passwd = conn.recv(1024)
-    ret = log_in_test(user_name.decode('utf-8'),passwd.decode('utf-8'))
-    conn.send(ret.encode('utf-8'))
-    if ret == "Log in successfully":
-        print(f"Connection form {ip_addr}")
-        while True:
-            # 接收客户端传递的数据，只接收1024个字节数据
-            res = conn.recv(1024)
-            if res.decode("utf-8") == 'bye':
+    user_name = conn.recv(1024)  # 接收客户端输入的用户名
+    if user_name.decode('utf-8') == "sign up":
+        while True:  # 循环注册
+            user_name = conn.recv(1024)
+            passwd = conn.recv(1024)
+            # 将注册信息写入数据库，若写入出现了问题还得让客户再换个用户名
+            sign_up_ret = newly_sign_up(user_name.decode('utf-8'),passwd.decode('utf-8'))
+            conn.send(sign_up_ret.encode('utf-8'))
+            if sign_up_ret == "Account has already existed.":
+                continue
+            else:
+                # 将这个用户名和密码写入数据库后，不用再验证合法性，返回信息后直接登入
+                ret = "Log in successfully"
+                conn.send(ret.encode('utf-8'))  # 将合法性返回给客户，便于其知晓自己的登入状态
+                if ret == "Log in successfully":  # 只有登入合法，才能继续进行信息接收操作
+                    print(f"Connection form {ip_addr}")  # 一个在server本端现实的连接信息
+                    echo_str_back(conn)  # 利用这个函数跟客户交互信息
+                else:
+                    continue
+                print("Disconnected.")
                 break
-            # 将客户端的数据接收到以后，转换成大写再编码后发送给客户端
-            conn.send(res.decode('utf-8').upper().encode('utf-8'))
     else:
-        continue
-    print("Disconnected.")
+        user_name = conn.recv(1024)
+        passwd = conn.recv(1024)    # 接收客户端输入的密码
+
+        ret = log_in_test(user_name.decode('utf-8'),passwd.decode('utf-8'))  # 用函数验证合法性
+        conn.send(ret.encode('utf-8'))      # 将合法性返回给客户，便于其知晓自己的登入状态
+        if ret == "Log in successfully":    # 只有登入合法，才能继续进行信息接收操作
+            print(f"Connection form {ip_addr}")     # 一个在server本端现实的连接信息
+            echo_str_back(conn)
+        else:
+            continue
+        print("Disconnected.")
+        break
     break
-
-
 
 # 关闭与客户端的连接
 conn.close()
