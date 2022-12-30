e368d9459d4f9ce521b969c134b2b20d3699fac2
Erutaner <renaissancezj@gmail.com>
2022-12-30 19:54:53 +0800

    已实现登录功能，传输内容仍是简单的echo

--- client_test.py
+++ client_test.py
@@ -10,16 +10,29 @@ client = socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
 
 # 连接服务端
 client.connect((MY_SERVER_IP,MY_SERVER_PORT))
-while True:
-    inp = input('>>>：').strip()
-    # 向服务端发送数据，需要转换成Bytes类型发送
-    client.send(inp.encode('utf-8'))
-    if inp == 'bye':
-        break
-    # 接收服务端回应给客户端的数据，不能超过1024字节
-    res = client.recv(1024)
+while True:  # 最外层循环用于登录
+    user_name = input('Please input your user name:').strip()
+    passwd = input("Please input your password:").strip()
+    client.send(user_name.encode('utf-8'))  # 将输入的用户名编码后进行传输
+    client.send(passwd.encode('utf-8'))  # 将输入的密码编码后传输
+    res = client.recv(1020)   # 接收服务器对账号密码判定的结果
+    print(res.decode('utf-8'))   # 解码后对本次登录结果输出
+    if res.decode('utf-8') == "Log in successfully":  # 若登录有效，则进入
+        while True:
+            inp = input('>>>：').strip()
+            # 向服务端发送数据，需要转换成Bytes类型发送
+            client.send(inp.encode('utf-8'))
+            if inp == 'bye':
+                break  # 双层循环唯一出口
+            # 接收服务端回应给客户端的数据，不能超过1024字节
+            res = client.recv(1024)
 
-    print(res.decode('utf-8'))
+            print(res.decode('utf-8'))
+    else:
+        continue
+    print("Disconnected.")
+    break
+
 
 # 套接字关闭
 client.close()
--- server_test.py
+++ server_test.py
@@ -16,16 +16,17 @@ def log_in_test(user_name,passwd):
     user_sql = f"select * from info where user_name = '{user_name}';"
     cursor.execute(user_sql)
     sel_result = cursor.fetchone()
-    print(f"the result is {sel_result}")
+    # print(f"the result is {sel_result}")  这行只是为了输出数据库中的命中记录，在实际运行中没必要
     if sel_result is not None:
         if sel_result[1] == passwd:
-            print("Log in successfully")
+            ret = "Log in successfully"
         else:
-            print('Access denied.')
+            ret = 'Access denied.'
     else:
-        print("Account doesn't exist.")
+        ret = "Account doesn't exist."
     cursor.close()
     sql_conn.close()
+    return ret
 
 # 创建套接字对象，AF_INET基于IPV4通信，SOCK_STREAM以数据流的形式传输数据，这里就可以确定是TCP了
 server = socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
@@ -39,16 +40,29 @@ server.bind((MY_SERVER_IP,MY_SERVER_PORT))
 server.listen(5)
 
 # 等待建立连接请求，会返回两个值，一个是连接状态，一个是连接的客户端IP与端口
+print("The server is waiting for connection...")
 conn,ip_addr = server.accept()
 
-
-while True:
+while True:  # 最外层循环用于登录
     # 接收客户端传递的数据，只接收1024个字节数据
-    res = conn.recv(10)
-    if res.decode("utf-8") == 'bye':
-        break
-    # 将客户端的数据接收到以后，转换成大写再编码后发送给客户端
-    conn.send(res.decode('utf-8').upper().encode('utf-8'))
+    user_name = conn.recv(1024)
+    passwd = conn.recv(1024)
+    ret = log_in_test(user_name.decode('utf-8'),passwd.decode('utf-8'))
+    conn.send(ret.encode('utf-8'))
+    if ret == "Log in successfully":
+        print(f"Connection form {ip_addr}")
+        while True:
+            # 接收客户端传递的数据，只接收1024个字节数据
+            res = conn.recv(1024)
+            if res.decode("utf-8") == 'bye':
+                break
+            # 将客户端的数据接收到以后，转换成大写再编码后发送给客户端
+            conn.send(res.decode('utf-8').upper().encode('utf-8'))
+    else:
+        continue
+    print("Disconnected.")
+    break
+
 
 
 # 关闭与客户端的连接
